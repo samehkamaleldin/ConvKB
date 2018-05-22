389785c002073dc1ec83cddd5b3676d2385c49fa
Sameh Mohamed <sameh.kamaleldin@gmail.com>
2018-05-22 17:57:08 +0100

    add dropout to get eval to work

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ class ConvKB(object):
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [batch_size, sequence_length], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [batch_size, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
--- train.py
+++ train.py
@@ -32,6 +32,7 @@ tf.flags.DEFINE_boolean("useConstantInit", False, "")
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
+
 print("\nParameters:")
 for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
