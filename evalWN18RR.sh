755fc95dcb7d1a4d00d8c04bd7efd9bdaa7c6577
Sameh Mohamed <sameh.kamaleldin@gmail.com>
2018-05-22 17:51:41 +0100

    change eval script for WN18RR

--- evalWN18RR.sh
+++ evalWN18RR.sh
@@ -1,8 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 0 &
-CUDA_VISIBLE_DEVICES=0 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 1 &
-CUDA_VISIBLE_DEVICES=0 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 2 &
-CUDA_VISIBLE_DEVICES=0 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 3 &
-CUDA_VISIBLE_DEVICES=1 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 4 &
-CUDA_VISIBLE_DEVICES=1 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 5 &
-CUDA_VISIBLE_DEVICES=1 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 6 &
-CUDA_VISIBLE_DEVICES=1 nohup python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 7 &
+#!/usr/bin/env bash
+
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 0 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 1 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 2 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 3 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 4 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 5 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 6 &
+python eval.py --embedding_dim 50 --num_filters 500 --name WN18RR --useConstantInit False --model_name wn18rr --num_splits 8 --testIdx 7 &
